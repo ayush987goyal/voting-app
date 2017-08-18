949c71a1a848fc6a9a45b17ef07148a3b585d3c0
Ayush Goyal <ayush987goyal@gmail.com>
2017-08-19 01:34:06 +0530

    updated server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/home", (req, res) => {
     res.send({msg: 'hello mate!'});
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0",  () => {
     var addr = server.address();
-    console.log("Server listening at port", addr.port);
+    console.log("Server listening at port", addr.address + ":" + addr.port);
 })
